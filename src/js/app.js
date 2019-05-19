c8e87872c04627f026ed14af97cb13420f6dac1b
odinokun <odinokun@gmail.com>
2019-05-19 10:24:29 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+$(function() {
+
+  // begin SVG for IE
+  require('./modules/svg4everybody')();
+
+  // begin no-JS
+  require('./modules/noJs')();
+
+  // begin Scroll2Ancor
+  // require('./modules/scroll2ancor')();
+
+  // begin Slick slider
+  // require('./modules/slick')();
+
+  // begin Popup
+  // require('./modules/popup')();
+
+  // begin Sticky header
+  // require('./modules/stickyHeader')();
+
+  // begin Animate.css
+  // require('./modules/waypoints')();
+  // require('./modules/animateCss')();
+
+  // begin Fancybox3
+  // require('./modules/fancybox')();
+
+});
